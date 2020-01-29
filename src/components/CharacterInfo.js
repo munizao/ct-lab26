d93ca95453387e47216bd18d157effc09fd4d3c8
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-28 16:53:30 -0800

    styled CharacterInfo div

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import styles from './App.css';
 import PropTypes from 'prop-types';
 import InfoLine from './InfoLine';
 
 
 const CharacterInfo = (props) => (
-  <>
+  <div className={styles.CharacterInfo}>
     <InfoLine lineHead="STATUS" value={props.status}/>
     <InfoLine lineHead="SPECIES" value={props.species}/>
     <InfoLine lineHead="GENDER" value={props.gender}/>
     <InfoLine lineHead="ORIGIN" value={props.origin}/>
     <InfoLine lineHead="LAST LOCATION" value={props.lastLocation}/>
-  </>
+  </div>
 );
 
 CharacterInfo.propTypes = {
